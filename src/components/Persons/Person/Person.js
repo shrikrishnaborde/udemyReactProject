ea46da88a50d870d4ed35eb81ae4a802d0de972e
Shrikrishna <sbbordeshrikrishna@gmail.com>
2020-12-26 11:43:57 +0530

    Project structure

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -3,21 +3,12 @@ import classes from './Person.css';
 import styled from 'styled-components';
 
 const person = (props) => {
-    const random = Math.random();
-
-    if(random < 0.7) {
-        throw new Error('Wrong happened');
-    }
-
     return (
             <div className={ classes.Person }>
                 <p onClick= {props.click}>I'm { props.name }!! and i am { props.age } years old</p>
                 <p> { props.children}</p>
                 <input type="text" onChange={props.changed} value ={ props.name}></input>
             </div>
-            // <StyledDiv>
-                
-            // </StyledDiv>
         )
     }
 
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import classes from './App.css';
 import React, { Component } from 'react';
-import Person  from './Person/Person';
-import ErrorBoundary from './ErrorBoundary/ErrorBoundary.js';
+import Person  from '../components/Persons/Person/Person';
+import ErrorBoundary from '../components/ErrorBoundary/ErrorBoundary.js';
 
 class App extends Component {
   state = {
